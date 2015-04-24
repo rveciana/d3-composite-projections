9d2629706b5b125f6ea4deae7d57ab8b65b07103
rveciana <rveciana@gmail.com>
2015-04-24 15:41:25 +0200

    A first real test (still very simple)

--- test/test.js
+++ test/test.js
@@ -1,9 +1,19 @@
 var assert = require("assert")
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
+var d3 = require('../bower_components/d3/d3.js'); 
+var composite_projection = require('../composite-projections.js'); 
+
+describe('Composite Projections', function(){
+  describe('Methods', function(){
+    it('All projections must be defined', function(){
+        assert.equal(typeof d3.geo.conicConformalSpain , 'function');
+     
+    })
+  });
+  describe('Spain', function(){
+    it('Projection results should be correct', function(){
+        assert.equal(typeof d3.geo.conicConformalSpain , 'function');
+       //assert.equal(composite_projection, );
       assert.equal(-1, [1,2,3].indexOf(0));
     })
   })
-})
+});  
