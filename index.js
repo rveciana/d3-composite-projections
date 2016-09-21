66bdfbdefbff0b8b0d37a6184ef1b37843d05a85
Roger Veciana i Rovira <rveciana@gmail.com>
2016-09-21 16:45:21 +0200

    conicConformalEurope added

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ export {default as geoMercatorEcuador} from "./src/mercatorEcuador";
 export {default as geoTransverseMercatorChile} from "./src/transverseMercatorChile";
 export {default as geoConicEquidistantJapan} from "./src/conicEquidistantJapan";
 export {default as geoConicConformalFrance} from "./src/conicConformalFrance";
+export {default as geoConicConformalEurope} from "./src/conicConformalEurope";
